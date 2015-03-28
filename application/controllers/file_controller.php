548670b88fb1cc94ba15220c7e9d5a9ea5875e4e
akhfa <akhmadfakhoni@gmail.com>
2015-03-29 06:54:53 +0700

    del unneded comment

--- application/controllers/file_controller.php
+++ application/controllers/file_controller.php
@@ -7,20 +7,7 @@
 	     parent::__construct();
 	     $this->load->helper('file');
 	   }
-	  //  function index()
-	  //  {
-	  //    if($this->session->userdata('logged_in'))
-	  //    {
-	  //      $session_data = $this->session->userdata('logged_in');
-	  //      $data['username'] = $session_data['username'];
-	  //      $data['id'] = $session_data['id'];
-	  //      $this->load->view('home_view', $data);
-	  //    }
-	  //    else
-	  //    {
-	  //      redirect('login', 'refresh');
-	  //    }
-	 	// }
+	   
 		function delFile()
 		{
 			$session_data = $this->session->userdata('logged_in');
@@ -51,13 +38,7 @@
 			$this->rrmdir($dir);
 
 			redirect('home', 'refresh');
-			
-			// $namaFile = $this->getNamaFile();
 
-			// if(unlink($dir))	
-				
-			// else
-			// 	echo "Delete Failed";
 		}
 
 		function getgroupname()
