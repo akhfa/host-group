36a76549c2da592b3432295543850567abd20d71
akhfa <akhmadfakhoni@gmail.com>
2015-03-29 15:44:11 +0700

    Adding database

--- db/trial_hostgroup.sql
+++ db/trial_hostgroup.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 29, 2015 at 02:16 PM
+-- Generation Time: Mar 29, 2015 at 03:11 PM
 -- Server version: 5.5.41-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.7
 
@@ -35,19 +35,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   `group` varchar(50) NOT NULL DEFAULT 'default',
   `role` varchar(50) NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`,`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `group`, `role`) VALUES
-(15, 'akhfa', '1b8ecafda6be41799698012eadf3f5f8', 'group2', 'admin'),
+(15, 'akhfa', '1b8ecafda6be41799698012eadf3f5f8', 'admin', 'admin'),
 (20, 'akhfa1', '1b8ecafda6be41799698012eadf3f5f8', 'grup1', 'user'),
 (21, 'akhfa2', '1b8ecafda6be41799698012eadf3f5f8', 'grup1', 'user'),
 (22, 'akhfa3', '1b8ecafda6be41799698012eadf3f5f8', 'grup2', 'user'),
 (23, 'akhfa4', '1b8ecafda6be41799698012eadf3f5f8', 'grup2', 'user'),
-(24, 'akhfa5', '1b8ecafda6be41799698012eadf3f5f8', 'default', 'user');
+(24, 'akhfa5', '1b8ecafda6be41799698012eadf3f5f8', 'default', 'user'),
+(26, 'akhfa5', '1b8ecafda6be41799698012eadf3f5f8', 'default', 'admin');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
