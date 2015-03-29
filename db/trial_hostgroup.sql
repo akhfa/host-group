c40a6c442050993e24a30afe423ed4ee8c384a94
akhfa <akhmadfakhoni@gmail.com>
2015-03-29 14:18:33 +0700

    Added database

--- db/trial_hostgroup.sql
+++ db/trial_hostgroup.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 25, 2015 at 03:46 PM
+-- Generation Time: Mar 29, 2015 at 02:16 PM
 -- Server version: 5.5.41-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.7
 
@@ -17,10 +17,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `trial_crudci`
+-- Database: `trial_hostgroup`
 --
-CREATE DATABASE IF NOT EXISTS `trial_crudci` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `trial_crudci`;
+CREATE DATABASE IF NOT EXISTS `trial_hostgroup` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `trial_hostgroup`;
 
 -- --------------------------------------------------------
 
@@ -31,19 +31,23 @@ USE `trial_crudci`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `group` varchar(50) NOT NULL DEFAULT 'default',
+  `role` varchar(50) NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`,`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'admin', '1b8ecafda6be41799698012eadf3f5f8'),
-(2, 'admin2', '1b8ecafda6be41799698012eadf3f5f8'),
-(3, 'admin3', 'fc79a73eeb9a04d128e5704d15c53531'),
-(4, 'admin4', '63dd3b371aff38f02d1f448c67d068dd');
+INSERT INTO `users` (`id`, `username`, `password`, `group`, `role`) VALUES
+(15, 'akhfa', '1b8ecafda6be41799698012eadf3f5f8', 'group2', 'admin'),
+(20, 'akhfa1', '1b8ecafda6be41799698012eadf3f5f8', 'grup1', 'user'),
+(21, 'akhfa2', '1b8ecafda6be41799698012eadf3f5f8', 'grup1', 'user'),
+(22, 'akhfa3', '1b8ecafda6be41799698012eadf3f5f8', 'grup2', 'user'),
+(23, 'akhfa4', '1b8ecafda6be41799698012eadf3f5f8', 'grup2', 'user'),
+(24, 'akhfa5', '1b8ecafda6be41799698012eadf3f5f8', 'default', 'user');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
